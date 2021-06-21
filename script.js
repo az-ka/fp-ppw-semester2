c230438aaaf2cf5cba8c7e0aad6916dde2ebec03
az-ka <82693758+az-ka@users.noreply.github.com>
2021-06-21 10:29:53 +0700

    update

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 function myNavbar() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar"
-    }
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
 }
 
 // Hide Show Fasilitas List
 
-const button = document.querySelector('#button');
-const show = document.querySelector('.show-more');
+const button = document.querySelector("#button");
+const show = document.querySelector(".show-more");
+let x = document.getElementById("hide");
 
-button.addEventListener('click', ()=> {
-    if(show.style.display === 'none'){
-        show.style.display = 'block';
-    }else{
-        show.style.display = 'none';
-    }
+button.addEventListener("click", () => {
+  if (show.style.display === "none") {
+    show.style.display = "block";
+    x.innerHTML = "Hide More";
+  } else {
+    show.style.display = "none";
+    x.innerHTML = "Show More";
+  }
 });
